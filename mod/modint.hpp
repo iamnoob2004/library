a1b8be4b6bf110284cc732c7607ca3515a8f5952
i_am_noob <albert.huang@i_am_noob>
2025-10-24 21:42:47 +0800

    haha

--- mod/modint.hpp
+++ mod/modint.hpp
@@ -1,30 +1,32 @@
 #pragma once
 
-template<int p>
+#include "library/nt/extgcd.hpp"
+
+template<int m,bool is_prime=true>
 struct modint{
     using mint=modint;
     using u32=uint32_t;
 
     static int get_mod(){
-        return p;
+        return m;
     }
 
     int x;
     modint():x(0){}
-    modint(int64_t _x):x((_x%p+p)%p){}
+    modint(int64_t _x):x((_x%m+m)%m){}
 
     mint operator += (const mint &o){
         x+=o.x;
-        if(x>=p) x-=p;
+        if(x>=m) x-=m;
         return *this;
     }
     mint operator -= (const mint &o){
         x-=o.x;
-        if(x<0) x+=p;
+        if(x<0) x+=m;
         return *this;
     }
     mint operator *= (const mint &o){
-        x=((int64_t)x)*o.x%p;
+        x=((int64_t)x)*o.x%m;
         return *this;
     }
     mint operator /= (const mint &o){
@@ -41,8 +43,17 @@ struct modint{
         for(; n; n>>=1,b*=b) if(n&1) res*=b;
         return res;
     }
+    inline mint inv1() const {
+        return pow(m-2);
+    }
+    inline mint inv2() const {
+        auto [g,val1,val2]=extgcd<int>(x,m);
+        assert(g==1);
+        return mint(val1);
+    }
     mint inv() const {
-        return pow(p-2);
+        if(is_prime) return inv1();
+        return inv2();
     }
 
     bool operator == (const mint &o) const {
